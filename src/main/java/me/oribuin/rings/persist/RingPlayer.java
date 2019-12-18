3064685678d9a69b8ab424759bf836e43252286b
ProSavage <naman0311@gmail.com>
2019-12-17 22:28:28 -0600

    1 ring at a time active

--- src/main/java/me/oribuin/rings/persist/RingPlayer.java
+++ src/main/java/me/oribuin/rings/persist/RingPlayer.java
@@ -5,66 +5,19 @@ import java.util.UUID;
 public class RingPlayer {
 
     private UUID uuid;
-    private boolean speedRing, healingRing, bruteRing, aquaRing, fireRing, strengthRing, visionRing;
+    private RingItemType activeRing;
 
 
     public RingPlayer(UUID uui) {
         this.uuid = uuid;
     }
 
-    public boolean isSpeedRing() {
-        return speedRing;
+    public RingItemType getActiveRing() {
+        return activeRing;
     }
 
-    public void setSpeedRing(boolean speedRing) {
-        this.speedRing = speedRing;
+    public void setActiveRing(RingItemType activeRing) {
+        this.activeRing = activeRing;
     }
 
-    public boolean isHealingRing() {
-        return healingRing;
-    }
-
-    public void setHealingRing(boolean healingRing) {
-        this.healingRing = healingRing;
-    }
-
-    public boolean isBruteRing() {
-        return bruteRing;
-    }
-
-    public void setBruteRing(boolean bruteRing) {
-        this.bruteRing = bruteRing;
-    }
-
-    public boolean isAquaRing() {
-        return aquaRing;
-    }
-
-    public void setAquaRing(boolean aquaRing) {
-        this.aquaRing = aquaRing;
-    }
-
-    public boolean isFireRing() {
-        return fireRing;
-    }
-
-    public void setFireRing(boolean fireRing) {
-        this.fireRing = fireRing;
-    }
-
-    public boolean isStrengthRing() {
-        return strengthRing;
-    }
-
-    public void setStrengthRing(boolean strengthRing) {
-        this.strengthRing = strengthRing;
-    }
-
-    public boolean isVisionRing() {
-        return visionRing;
-    }
-
-    public void setVisionRing(boolean visionRing) {
-        this.visionRing = visionRing;
-    }
 }
